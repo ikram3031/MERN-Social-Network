49ead54e1c961693d5b5a06c0e9fa7aa48f7b15e
ikram3031 <mdikram.3031@gmail.com>
2023-12-24 22:39:34 +0600

    6:40

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div classname="container">
+            <div classname="row">
+                <div classname="col">
+                    <h1>Home</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
